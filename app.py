182a8b35cdf4e9fed154058f37f75c4f72451602
root <midnightlunalux@gmail.com>
2023-04-14 21:36:20 -0400

    generating first set of survey questions + some css styling

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import surveys
+from surveys import satisfaction_survey
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -8,3 +8,25 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "very_secret"
 debug = DebugToolbarExtension(app)
 
+responses = []
+
+@app.route('/')
+def welcome():
+    title = satisfaction_survey.title
+    instructions = satisfaction_survey.instructions
+    return render_template('survey_home.html', survey_title = title, instructions = instructions)
+
+@app.route('/questions/<num>')
+def show_form(num):
+    num = int(num)
+    question = satisfaction_survey.questions[num].question
+    choices = satisfaction_survey.questions[num].choices
+    num += 1
+    return render_template('questionnaire.html', question = question, choice0 = choices[0], choice1 = choices[1])
+
+@app.route('/questions/submit', methods=["POST"])
+def submit_data():
+    look_at_args = request.args["option"]
+    responses.append(look_at_args)
+    return 
+    
