bf7a8ef37d8e1ca9fce710c83aea2d69f7b3f480
root <midnightlunalux@gmail.com>
2023-04-15 17:29:22 -0400

    Adding a /completed route, adding logic to prevent jumping around questions and error messages.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, flash
 from surveys import satisfaction_survey as survey
 
 from flask_debugtoolbar import DebugToolbarExtension
@@ -14,17 +14,34 @@ responses = []
 
 @app.route('/')
 def welcome():
+    """Shows home page"""
     return render_template('survey_home.html', survey = survey)
 
 @app.route('/questions/<int:num>')
 def show_form(num):
+    """Take users through questions and redirecting if number of responses does not correlate to question number"""
+    if not len(responses) == num:
+        flash("Invalid action", "error")
+        return redirect(f'/questions/{len(responses)}')
+    if num == len(survey.questions):
+        return redirect('/completed')
+    
+    num = len(responses)
     question = survey.questions[num]
-    num += 1
     return render_template('questionnaire.html', num = num, question = question)
 
 @app.route('/submit', methods=["POST"])
 def submit_data():
+    """appends data to responses list"""
     args = request.form["option"]
     responses.append(args)
     return redirect(f'/questions/{len(responses)}')
     
+@app.route('/completed')
+def completed():
+    """Check if user has completed the survey. if so, user is thanked and will not be able to re-complete it. If not, user is redirected to survey"""
+
+    if not len(responses) == len(survey.questions):
+        flash ("Please finish the survey", "error")
+        return redirect(f'/questions/{len(responses)}')
+    return render_template('/completed.html')
